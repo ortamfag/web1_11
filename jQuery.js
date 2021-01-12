079e82a79c497458801c2f67f4aa9ff451b340e2
ortamfag <a_zhidov@outlook.com>
2021-01-12 23:17:16 +0300

    5 methods

---
+++ jQuery.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+   $("#hideheader").click(function(){
+       $(".picture").hide();
+   })
+
+   $("#showheader").click(function(){
+       $(".picture").show();
+
+    
+   $("#hidemain").click(function(){
+       $(".imagee").toggle();
+   })
+
+   $("#Loremcolor").click(function(){
+       $(".lines").addClass("blue");
+   })
+
+   $('#btn').click(function(){
+	var html = $('.example').text();
+
+       alert(html);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+   })
+   //сделать кнопки читабельными(25line);
+
+
+
+
+
+
+
+
+
+
+});
+
