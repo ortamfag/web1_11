670d97979d11e6b9e2e10c9bb53e31c30da6376a
ortamfag <a_zhidov@outlook.com>
2021-01-13 00:33:25 +0300

    14 methods

--- jQuery.js
+++ jQuery.js
@@ -2,26 +2,48 @@ $(document).ready(function(){
    $("#hideheader").click(function(){
        $(".picture").hide();
    })
-
+5
    $("#showheader").click(function(){
        $(".picture").show();
 
-    
+    })
    $("#hidemain").click(function(){
        $(".imagee").toggle();
    })
 
-   $("#Loremcolor").click(function(){
+   $("#Loremcolor").dblclick(function(){
        $(".lines").addClass("blue");
    })
 
    $('#btn').click(function(){
-	var html = $('.example').text();
-
-       alert(html);
-
+	alert("Value: " + $("#test").val());
 });
 
+    $("#slide").dblclick(function(){
+        $("#nextslide").slideDown("slow");
+    });
+
+    $("#slide1").dblclick(function(){
+        $("#nextslide1").slideToggle('slow');
+    });
+
+    $(".focus").mouseenter(function(){
+        $(".focus").fadeOut('slow');
+    })
+
+    $(".focus2").mouseenter(function(){
+        $(".focus2").animate({
+            left: '400px',
+            opacity: '0.5',
+            height: '150px',
+            width: '150px'})
+    })
+
+    $(".last_anim").dblclick(function(){
+        $(".focus3").fadeToggle();
+        $(".focus3_1").fadeToggle('slow');
+        $(".focus3_2").fadeToggle(3000);
+    })
 
 
 
@@ -33,7 +55,10 @@ $(document).ready(function(){
 
 
 
-   })
+
+
+
+   
    //сделать кнопки читабельными(25line);
 
 
